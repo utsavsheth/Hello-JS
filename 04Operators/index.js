94f6069a15f1ae65435623a3cd61e25fb915b3fc
utsavsheth <iamutsavsheth@gmail.com>
2022-04-24 11:05:07 +0530

    Operators + Math

--- 04Operators/index.js
+++ 04Operators/index.js
@@ -1,4 +1,19 @@
-var num1 = 43;
-var num2 = 23;
+// var num1 = 43;
+// var num2 = 23;
 
-console.log(num1 * num2);
+// console.log(num1 * num2);
+
+// // num1 should be written like this - num1, it's not 1num.
+
+var sellingPrice = 199;
+var listingPrice = 799;
+
+var discountPercent = ((listingPrice - sellingPrice) / listingPrice) * 100;
+
+console.log ("Discount percentage is:" + discountPercent);
+
+var displayDiscountPercentage = Math.round(discountPercent);
+
+console.log(displayDiscountPercentage + "% OFF");
+
+// I learn calculation of price
